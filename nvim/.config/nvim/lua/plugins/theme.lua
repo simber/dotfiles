1c62929c773ae433f38d380e822ba2c966947218
Simon Berger <simon.berger@hey.com>
2026-03-28 22:30:56 +0100

    Do not simlink theme file

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,1 +1,12 @@
-/home/borgman/.config/omarchy/current/theme/neovim.lua
+return {
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}

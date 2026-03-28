58554ba756d8823a9398a284700d5a800113b0d9
Simon Berger <simon.berger@hey.com>
2026-03-28 19:37:15 +0100

    Fix startship

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ mkdir ~/.config
 cd ~/dotfiles
 cp ~/dotfiles/starship/.config/starship.toml ~/.config/starship.toml
 sed -i 's/(#a3aed2)\\/(#f7768e)\\/g' ~/.config/starship.toml
-sed -i 's/󰕈 ](bg:#a3aed2/󰣀 ](bg:#f7768e/g' ~/.config/starship.toml
+sed -i 's/󰣇 ](bg:#a3aed2/󰣀 ](bg:#f7768e/g' ~/.config/starship.toml
 sed -i 's/fg:#a3aed2)/fg:#f7768e)/g' ~/.config/starship.toml
 stow nvim
 stow bashdev

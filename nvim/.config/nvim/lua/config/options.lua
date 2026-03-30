3da91c7c1eee04131a581c59537e4d4818ac2bd8
Simon Berger <simon.berger@hey.com>
2026-03-30 15:56:11 +0200

    clipboard working in devcontainer

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,9 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.relativenumber = true
-vim.opt.wrap = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.expandtab = true
-vim.opt.clipboard = ""
+--
+
+-- Set all indentation sizes to 2 (example)
+vim.opt.tabstop = 4 -- how many spaces a <Tab> is
+vim.opt.shiftwidth = 4 -- indentation level for >> << and auto indent
+vim.opt.softtabstop = 4 -- how many spaces <Tab> feels like in insert mode
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.g.autoformat_disabled_filetypes = { "sh" }
+
+vim.opt.title = true
+vim.opt.titlestring = "nvim"
+vim.opt.titleold = "shell"
+vim.g.clipboard = "osc52"
